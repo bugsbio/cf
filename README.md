46c6f4ae5883944a8aa962b66bdb083b462b9078
Russell <russell@russelldunphy.com>
2015-07-13 16:22:16 +0100

    :hear_no_evil: Add pom to gitignore

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
-If the jenkins box doesn't come up, it's probably something to do with the EBS volume.
-Go into the jenkins directory and run `make attach-volume` to attempt to fix it.
+# cf
 
-# Web #
--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject cf "0.1.0-SNAPSHOT"
   :description "DSL for building Cloudformation JSON as a Clojure map"
   :url "http://github.com/bugsbio/cf"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
+  :signing {:gpg-key "CF73E6ED"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [selmer "0.8.0"]
                  [cheshire "5.4.0"]]
